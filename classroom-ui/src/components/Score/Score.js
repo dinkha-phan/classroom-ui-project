80a29232123ec352443c6201d75cf347cba6126d
ntdtiendat712 <ntdtiendat712@gmail.com>
2021-12-31 00:07:45 +0700

    minor fix, clean code

--- classroom-ui/src/components/Score/Score.js
+++ classroom-ui/src/components/Score/Score.js
@@ -33,15 +33,15 @@ import { visuallyHidden } from '@mui/utils';
 import Button from '@mui/material/Button';
 
 
-function createData(name, calories, fat, carbs, protein) {
-    return {
-        name,
-        calories,
-        fat,
-        carbs,
-        protein,
-    };
-}
+// function createData(name, calories, fat, carbs, protein) {
+//     return {
+//         name,
+//         calories,
+//         fat,
+//         carbs,
+//         protein,
+//     };
+// }
 
 // const rows = [
 //     createData('Cupcake', 305, 3.7, 67, 4.3),
@@ -57,183 +57,6 @@ function createData(name, calories, fat, carbs, protein) {
 //     createData('Marshmallow', 318, 0, 81, 2.0),
 //     createData('Nougat', 360, 19.0, 9, 37.0),
 //     createData('Oreo', 437, 18.0, 63, 4.0),
-// ];
-
-
-function descendingComparator(a, b, orderBy) {
-    if (b[orderBy] < a[orderBy]) {
-        return -1;
-    }
-    if (b[orderBy] > a[orderBy]) {
-        return 1;
-    }
-    return 0;
-}
-
-function getComparator(order, orderBy) {
-    return order === 'desc'
-        ? (a, b) => descendingComparator(a, b, orderBy)
-        : (a, b) => -descendingComparator(a, b, orderBy);
-}
-
-// This method is created for cross-browser compatibility, if you don't
-// need to support IE11, you can use Array.prototype.sort() directly
-function stableSort(array, comparator) {
-    const stabilizedThis = array.map((el, index) => [el, index]);
-    stabilizedThis.sort((a, b) => {
-        const order = comparator(a[0], b[0]);
-        if (order !== 0) {
-            return order;
-        }
-        return a[1] - b[1];
-    });
-    return stabilizedThis.map((el) => el[0]);
-}
-
-const headCells = [
-    {
-        id: 'name',
-        numeric: false,
-        disablePadding: true,
-        label: 'Dessert (100g serving)',
-    },
-    {
-        id: 'calories',
-        numeric: true,
-        disablePadding: true,
-        label: 'Calories',
-    },
-    {
-        id: 'fat',
-        numeric: true,
-        disablePadding: true,
-        label: 'Fat (g)',
-    },
-    {
-        id: 'carbs',
-        numeric: true,
-        disablePadding: true,
-        label: 'Carbs (g)',
-    },
-    {
-        id: 'protein',
-        numeric: true,
-        disablePadding: true,
-        label: 'Protein (g)',
-    },
-];
-const handleSave = (e, index, row) => {
-    console.log("edit", e, index, row);
-}
-function EnhancedTableHead(props) {
-    const { onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort } =
-        props;
-    const createSortHandler = (property) => (event) => {
-        onRequestSort(event, property);
-    };
-
-    return (
-        <TableHead>
-            <TableRow>
-                <TableCell padding="none">
-                    <Checkbox
-                        color="primary"
-                        indeterminate={numSelected > 0 && numSelected < rowCount}
-                        checked={rowCount > 0 && numSelected === rowCount}
-                        onChange={onSelectAllClick}
-                        inputProps={{
-                            'aria-label': 'select all desserts',
-                        }}
-                    />
-                </TableCell>
-                {headCells.map((headCell) => (
-                    <TableCell
-                        key={headCell.id}
-                        align={headCell.numeric ? 'center' : 'left'}
-                        padding={headCell.disablePadding ? 'none' : 'normal'}
-                        sortDirection={orderBy === headCell.id ? order : false}
-                    >
-                        <TableSortLabel
-                            active={orderBy === headCell.id}
-                            direction={orderBy === headCell.id ? order : 'asc'}
-                            onClick={createSortHandler(headCell.id)}
-                        >
-                            {headCell.label}
-                            {orderBy === headCell.id ? (
-                                <Box component="span" sx={visuallyHidden}>
-                                    {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
-                                </Box>
-                            ) : null}
-                        </TableSortLabel>
-                    </TableCell>
-                ))}
-            </TableRow>
-        </TableHead>
-    );
-}
-
-EnhancedTableHead.propTypes = {
-    numSelected: PropTypes.number.isRequired,
-    onRequestSort: PropTypes.func.isRequired,
-    onSelectAllClick: PropTypes.func.isRequired,
-    order: PropTypes.oneOf(['asc', 'desc']).isRequired,
-    orderBy: PropTypes.string.isRequired,
-    rowCount: PropTypes.number.isRequired,
-};
-const EnhancedTableToolbar = (props) => {
-    const { numSelected } = props;
-
-    return (
-        <Toolbar
-            sx={{
-                pl: { sm: 2 },
-                pr: { xs: 1, sm: 1 },
-                ...(numSelected > 0 && {
-                    bgcolor: (theme) =>
-                        alpha(theme.palette.primary.main, theme.palette.action.activatedOpacity),
-                }),
-            }}
-        >
-            {numSelected > 0 ? (
-                <Typography
-                    sx={{ flex: '1 1 100%' }}
-                    color="inherit"
-                    variant="subtitle1"
-                    component="div"
-                >
-                    {numSelected} selected
-                </Typography>
-            ) : (
-                <Typography
-                    sx={{ flex: '1 1 100%' }}
-                    variant="h6"
-                    id="tableTitle"
-                    component="div"
-                >
-                    Nutrition
-                </Typography>
-            )}
-
-            {numSelected > 0 ? (
-                <Tooltip title="Delete">
-                    <IconButton>
-                        <DeleteIcon />
-                    </IconButton>
-                </Tooltip>
-            ) : (
-                <Tooltip title="Filter list">
-                    <IconButton>
-                        <FilterListIcon />
-                    </IconButton>
-                </Tooltip>
-            )}
-        </Toolbar>
-    );
-};
-
-EnhancedTableToolbar.propTypes = {
-    numSelected: PropTypes.number.isRequired,
-};
 
 
 const getScoreBoard = () => {
@@ -276,6 +99,15 @@ export default function Score({ classData }) {
     // const listStudents = [{ name: "Phan Dinh Kha" }, { name: "Nguyen Tien Dat" }, { name: "Tran Bao Nguyen" }];
     const rows = getScoreBoard();
     const listHeadLabel = getlistHeadLabel();
+    const handleSave = (e, index, row) => {
+        console.log("StudenId:", row.StudenID);
+        console.log("label:", listHeadLabel[index]);
+        console.log("value", e.value);
+        row[listHeadLabel[index]] = e.value;
+        console.log("event:", e);
+        console.log("headIndex:", index);
+        console.log("row:", row);
+    }
     const handleForce = (data, fileInfo) => {
         // console.log(data, fileInfo);
         const token = getAccessToken();
@@ -317,177 +149,7 @@ export default function Score({ classData }) {
     }, []);
 
 
-    const handleRequestSort = (event, property) => {
-        const isAsc = orderBy === property && order === 'asc';
-        setOrder(isAsc ? 'desc' : 'asc');
-        setOrderBy(property);
-    };
 
-    const handleSelectAllClick = (event) => {
-        if (event.target.checked) {
-            const newSelecteds = rows.map((n) => n.name);
-            // setSelected(newSelecteds);
-            return;
-        }
-        setSelected([]);
-    };
-
-    const handleClick = (event, name) => {
-        const selectedIndex = selected.indexOf(name);
-        let newSelected = [];
-
-        if (selectedIndex === -1) {
-            newSelected = newSelected.concat(selected, name);
-        } else if (selectedIndex === 0) {
-            newSelected = newSelected.concat(selected.slice(1));
-        } else if (selectedIndex === selected.length - 1) {
-            newSelected = newSelected.concat(selected.slice(0, -1));
-        } else if (selectedIndex > 0) {
-            newSelected = newSelected.concat(
-                selected.slice(0, selectedIndex),
-                selected.slice(selectedIndex + 1),
-            );
-        }
-
-        // setSelected(newSelected);
-    };
-
-    const handleChangePage = (event, newPage) => {
-        setPage(newPage);
-    };
-
-    const handleChangeRowsPerPage = (event) => {
-        setRowsPerPage(parseInt(event.target.value, 10));
-        setPage(0);
-    };
-
-    const handleChangeDense = (event) => {
-        setDense(event.target.checked);
-    };
-
-    const isSelected = (name) => selected.indexOf(name) !== -1;
-
-    // Avoid a layout jump when reaching the last page with empty rows.
-    const emptyRows =
-        page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
-    // return (
-    //     <>
-
-
-    //         <Box sx={{ width: '100%' }}>
-    //             <Paper sx={{ width: '100%', mb: 2 }}>
-    //                 <EnhancedTableToolbar numSelected={selected.length} />
-    //                 <TableContainer>
-    //                     <Table
-    //                         sx={{ minWidth: 750 }}
-    //                         aria-labelledby="tableTitle"
-    //                         size={dense ? 'small' : 'medium'}
-    //                     >
-    //                         <EnhancedTableHead
-    //                             numSelected={selected.length}
-    //                             order={order}
-    //                             orderBy={orderBy}
-    //                             onSelectAllClick={handleSelectAllClick}
-    //                             onRequestSort={handleRequestSort}
-    //                             rowCount={rows.length}
-    //                         />
-    //                         <TableBody>
-    //                             {/* if you don't need to support IE11, you can replace the `stableSort` call with:
-    //              rows.slice().sort(getComparator(order, orderBy)) */}
-    //                             {stableSort(rows, getComparator(order, orderBy))
-    //                                 .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-    //                                 .map((row, index) => {
-    //                                     const isItemSelected = isSelected(row.name);
-    //                                     const labelId = `enhanced-table-checkbox-${index}`;
-    //                                     console.log("row, index", row, index);
-    //                                     return (
-    //                                         <TableRow
-    //                                             hover
-    //                                             onClick={(event) => handleClick(event, row.name)}
-    //                                             role="checkbox"
-    //                                             aria-checked={isItemSelected}
-    //                                             tabIndex={-1}
-    //                                             key={row.name}
-    //                                             selected={isItemSelected}
-    //                                             padding="none"
-    //                                         >
-    //                                             <TableCell padding="none">
-    //                                                 <Checkbox
-    //                                                     color="primary"
-    //                                                     checked={isItemSelected}
-    //                                                     inputProps={{
-    //                                                         'aria-labelledby': labelId,
-    //                                                     }}
-    //                                                 />
-    //                                             </TableCell>
-    //                                             <TableCell
-    //                                                 component="th"
-    //                                                 id={labelId}
-    //                                                 scope="row"
-    //                                                 padding="none"
-    //                                             >                              {row.name}
-    //                                             </TableCell>
-    //                                             <TableCell align="center"
-    //                                                 padding="none"
-    //                                             >
-    //                                                 <div style={{
-    //                                                     display: "flex", justifyContent: "center",
-    //                                                     alignItems: "center",
-    //                                                     height: "100%",
-    //                                                     margin: 0,
-    //                                                     padding: 0
-    //                                                 }}>
-    //                                                     <EditText style={{
-    //                                                         display: "flex",
-    //                                                         textAlign: "center",
-    //                                                         alignItems: "center",
-    //                                                         justifyContent: "center",
-    //                                                         paddingTop: 6,
-    //                                                         paddingBottom: 6,
-    //                                                         margin: 0,
-    //                                                         borderWidth: 0,
-    //                                                         width: "100px",
-    //                                                     }}
-    //                                                         defaultValue={row.calories}
-    //                                                         onSave={(e) => handleSave(e, index, row)} />
-    //                                                 </div>
-    //                                             </TableCell>
-    //                                             <TableCell align="center" padding="none">{row.fat}</TableCell>
-    //                                             <TableCell align="center" padding="none">{row.carbs}</TableCell>
-    //                                             <TableCell align="center" padding="none">{row.protein}</TableCell>
-    //                                         </TableRow>
-    //                                     );
-    //                                 })}
-    //                             {emptyRows > 0 && (
-    //                                 <TableRow
-    //                                     style={{
-    //                                         height: (dense ? 33 : 53) * emptyRows,
-    //                                     }}
-    //                                 >
-    //                                     <TableCell colSpan={6} />
-    //                                 </TableRow>
-    //                             )}
-    //                         </TableBody>
-    //                     </Table>
-    //                 </TableContainer>
-    //                 <TablePagination
-    //                     rowsPerPageOptions={[5, 10, 25]}
-    //                     component="div"
-    //                     count={rows.length}
-    //                     rowsPerPage={rowsPerPage}
-    //                     page={page}
-    //                     onPageChange={handleChangePage}
-    //                     onRowsPerPageChange={handleChangeRowsPerPage}
-    //                 />
-    //             </Paper>
-    //             <FormControlLabel
-    //                 control={<Switch checked={dense} onChange={handleChangeDense} />}
-    //                 label="Dense padding"
-    //             />
-    //         </Box>
-
-    //     </>
-    // );
     return (
         <>
 
@@ -504,10 +166,12 @@ export default function Score({ classData }) {
                         inputId="ObiWan"
                         inputName="ObiWan"
                         inputStyle={{ color: 'red' }}
-                    />
+                    >
+
+                    </CSVReader>
                     <CSVLink data={csvData} style={{ textDecoration: "none" }}>
                         <Button variant="contained" startIcon={<DownloadIcon />}>
-                            <p style={{ textDecoration: "none" }}>Download</p>
+                            Download
                         </Button>
                     </CSVLink>
                 </div>
@@ -528,9 +192,10 @@ export default function Score({ classData }) {
                             </TableRow>
                         </TableHead>
                         <TableBody>
-                            {rows.map((row) => (
+                            {rows.map((row, indexRow) => (
                                 <TableRow
-                                    key={row.StudenID}
+                                    // key={row.StudenID}
+                                    // key={indexRow}
                                     sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                                 >
                                     {listHeadLabel.map((value, index) => {
@@ -554,6 +219,7 @@ export default function Score({ classData }) {
                                                     borderWidth: 0,
                                                     width: "100px",
                                                 }}
+                                                    name={`Edit value row:${indexRow} label:${listHeadLabel[index]} `}
                                                     readonly={index === 0}
                                                     defaultValue={row[value]}
                                                     onSave={(e) => handleSave(e, index, row)} />
